cd1727cf7af307ba1c594d9b214a4c2b37d76dc4
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2021-12-10 02:31:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var csv = require("fast-csv");
-const uri = "mongodb+srv://skygoaleducation:skygoaleducation@priaynaka-saree-mandir.98lj8.mongodb.net/priaynaka_saree_mandir?retryWrites=true&w=majority";
+const uri = "<YOUR_DATABASE_URL_HERE>";
 var MongoClient = require('mongodb').MongoClient;
 
 var dataArr = [];
-csv.parseFile("cus.csv", {headers: true})
+csv.parseFile("<CSV_FILE_PATH_HERE>", {headers: true})
 .on("data", data => {
   dataArr.push(data);
 })
@@ -16,9 +16,9 @@ console.log('hello')
 
 MongoClient.connect(uri, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("priaynaka_saree_mandir");
+    var dbo = db.db("<YOUR_DB_NAME>");
     
-    dbo.collection("customer").insertMany(dataArr, function(err, res) {
+    dbo.collection("<YOUR_COLLECTION_NAME>").insertMany(dataArr, function(err, res) {
       if (err) throw err;
       console.log("Number of documents inserted: " + res.insertedCount);
       db.close();
